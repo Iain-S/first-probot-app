ac3866d331eaefe4b0cffd56c16c7c8b8529e12e
Iain-S <25081046+Iain-S@users.noreply.github.com>
2021-07-15 15:37:29 +0100

    Update greeting message and add .idea files

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Probot } from "probot";
 export = (app: Probot) => {
   app.on("issues.opened", async (context) => {
     const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+      body: "Cheers for opening this issue!",
     });
     await context.octokit.issues.createComment(issueComment);
   });
